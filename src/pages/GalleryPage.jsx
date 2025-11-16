20f82eec1f11eac352fd674cc5e16cb1726242ca
gaurav-com-1 <gauravacharya097@gmail.com>
2025-11-16 14:43:35 +0545

    Fix 4

--- src/pages/GalleryPage.jsx
+++ src/pages/GalleryPage.jsx
@@ -2,7 +2,6 @@
 import { useState } from 'react';
 import { motion } from 'framer-motion';
 import Lightbox from "yet-another-react-lightbox";
-import "yet-another-react-lightbox/styles.css";
 
 // Import your photos
 import photo1 from '../assets/photo1.jpg';
